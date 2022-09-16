70c9b87691ee95ca6639e53e38fa51d9ee0a6091
amsaabesh <mahiraabesh@gmail.com>
2022-09-17 02:13:33 +0600

    add comment

--- Insertion sort/Program.cs
+++ Insertion sort/Program.cs
@@ -6,23 +6,20 @@ namespace Insertion_sort
     {
         static void Main(string[] args)
         {
-            int[] arr = new int[10]; 
-            int n = 10, i, j, val, flag;
+            int n, i, j, val, flag;
+            Console.WriteLine("Number of array?");
+            n = Convert.ToInt32(Console.ReadLine());//convert string to number
+            int[] arr = new int[n];//array size
             Console.WriteLine("Enter Numbers");
-            for (i = 0; i < 10; i++)
-            {
-                arr[i] = Convert.ToInt32(Console.ReadLine()); //insert array
-            }
-            Console.Write("Initial array is: ");
             for (i = 0; i < n; i++)
             {
-                Console.Write(arr[i] + " ");
+                arr[i] = Convert.ToInt32(Console.ReadLine()); //insert array
             }
             for (i = 1; i < n; i++) //sorting
             {
                 val = arr[i];
                 flag = 0;
-                for (j = i - 1; j >= 0 && flag != 1;)
+                for (j = i - 1; j >= 0 && flag != 1;)//main algorithm
                 {
                     if (val < arr[j])
                     {
