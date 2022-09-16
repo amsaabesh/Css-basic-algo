7f7e6de097b5ca03000a455026902c134be76eb1
amsaabesh <mahiraabesh@gmail.com>
2022-09-16 13:20:11 +0600

    insertion sort

---
+++ Insertion sort/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Insertion_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[10]; 
+            int n = 10, i, j, val, flag;
+            Console.WriteLine("Enter Numbers");
+            for (i = 0; i < 10; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine()); //insert array
+            }
+            Console.Write("Initial array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            for (i = 1; i < n; i++) //sorting
+            {
+                val = arr[i];
+                flag = 0;
+                for (j = i - 1; j >= 0 && flag != 1;)
+                {
+                    if (val < arr[j])
+                    {
+                        arr[j + 1] = arr[j];
+                        j--;
+                        arr[j + 1] = val;
+                    }
+                    else flag = 1;
+                }
+            }
+            Console.WriteLine("\nSorted Array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
