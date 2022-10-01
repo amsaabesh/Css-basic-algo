fe322b335506712243eaaa811b9ac40326e76dc2
amsaabesh <mahiraabesh@gmail.com>
2022-10-01 17:36:15 +0600

    add selection sort

---
+++ Selection Sort/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Selection_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, i, j;
+            Console.WriteLine("Array Size?");
+            n = Convert.ToInt32(Console.ReadLine());//convert string to number
+            int[] arr = new int[n]; //array size
+            for (i = 0; i < n; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine()); //insert array
+            }
+            Console.Write("Initial array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            int temp, smallest;
+            for (i = 0; i < n - 1; i++)
+            {
+                smallest = i; //find the min element in unsorted array
+                for (j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[smallest])
+                    {
+                        smallest = j;
+                    }
+                }
+                //swap min with first val.
+                temp = arr[smallest];
+                arr[smallest] = arr[i];
+                arr[i] = temp;
+            }
+            Console.WriteLine();
+            Console.Write("Sorted array is: ");
+            for (i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
+    }
+}
